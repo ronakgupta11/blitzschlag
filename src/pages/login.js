cf778bd303632b14114a3863262fddd8be9852e4
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-12-25 14:21:07 +0530

    login sign up fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -34,8 +34,8 @@ const num = Math.floor(Math.random() * 5) % 5
       ></div>
     
       <div className='fixed top-0 left-0 w-full h-screen bg-gradient-to-b from-[#070707] opacity-70 to-transparent z-10'></div>
-    <div className='relative overfolw-x-hidden z-10  py-12 flex flex-col lg:flex-row items-center justify-around space-y-4 w-full'>
-<div className='font-black-ops-one text-white text-5xl items-center '>
+    <div className='relative overfolw-x-hidden z-10  py-12 flex flex-col lg:flex-row items-center justify-around space-y-4 w-[80%] m-auto'>
+<div className='font-black-ops-one text-white md:text-5xl text-4xl items-center '>
 BLITZSCHLAG
 </div>
 <div className=' bg-black opacity-80 backdrop-blur-md rounded-lg p-8'>
--- src/pages/register.js
+++ src/pages/register.js
@@ -31,11 +31,11 @@ const auth = useSelector(selectAuthenticated)
       ></div>
     
       <div className='fixed top-0 left-0 w-full h-screen bg-gradient-to-b from-[#070707] opacity-70 to-transparent z-10'></div>
-    <div className='relative overfolw-hidden  z-10  py-12 flex flex-col lg:flex-row items-center justify-around space-y-4 w-full'>
-<div className='font-black-ops-one text-white text-5xl items-center lg:h-screen '>
+    <div className='relative overfolw-hidden  z-10  py-12 flex flex-col lg:flex-row items-center justify-around space-y-4 w-[80%] m-auto'>
+<div className='font-black-ops-one text-white text-4xl md:text-5xl items-center lg:h-screen '>
 BLITZSCHLAG
 </div>
-<div className=' bg-black opacity-80 backdrop-blur-md w-96 lg:w-[350px] rounded-lg p-8'>
+<div className=' bg-black opacity-80 backdrop-blur-md  lg:w-[350px] rounded-lg p-8'>
   
   <p className='text-white text-xl font-black-ops-one my-4'> SIGN UP</p>
   <SignupForm/>
