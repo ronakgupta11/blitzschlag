df37f03cd2d67da9308b65ddc7dcb1ddfc36f81e
Tryptophan69 <2021ume1097@mnit.ac.in>
2024-01-04 16:11:48 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,33 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='nav w-full h-64 '>
-
+    <div className='nav py-5 w-full flex bg-[#0A0A0A] opacity-[0.85]'>
+    <ul className='w-full flex justify-evenly align-middle text-white text-center font-black-ops-one text-[16px] font-normal leading-[44px]' >
+      <li>
+  
+      </li>
+      <li>
+      <a href="">Home</a>
+      </li>
+      <li>
+      <a href="">Events</a>
+      </li>
+      <li>
+      <a href="">Contact Us</a>
+      </li>
+      <li className='flex flex-col justify-center align-middle'>
+       <button className='bg-[#463000] btn-sm lg:btn text-white lg:px-6 text-center flex flex-col justify-center align-middle rounded-full'>
+        <a className='text-[16px]' href="">Register</a>
+       </button>
+      </li>
+      <li className='flex flex-col align-middle justify-center'>
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="18" viewBox="0 0 32 18" fill="none">
+  <rect width="16" height="3.2" rx="1.6" fill="white"/>
+  <rect x="16" y="14.4004" width="16" height="3.2" rx="1.6" fill="white"/>
+  <rect y="7.2002" width="32" height="3.2" rx="1.6" fill="white"/>
+</svg>
+      </li>
+    </ul>
     </div>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@
 import Footer from "./Footer";
 import React from "react";
 import { ParallaxProvider } from "react-scroll-parallax";
+import Navbar from "./Navbar";
 
 function Layout({ children }) {
   return (
@@ -10,7 +11,9 @@ function Layout({ children }) {
 
     <div className="layout ">
       {/* <div className="sidebar fixed z-[9999999]  "><Sidebar/></div> */}
-
+      <div className="fixed  w-full z-[99]">
+        <Navbar/>
+      </div>
       <div className="content flex-1 w-full ">{children}</div>
       <Footer />
     </div>
