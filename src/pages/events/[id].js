16b8f0831010fb1a30629cddb9768c04b271bb7c
Ronak Gupta <mailtoronakgupta@gmail.com>
2024-01-05 13:57:51 +0530

    mobile device

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -1,18 +1,25 @@
 import React, { useEffect } from "react";
 import MobileEventPage from "../../components/eventsComponents/MobileEventPage";
 import DesktopEventPage from "/src/components/eventsComponents/DesktopEventPage";
-
+import { UAParser } from "ua-parser-js";
 function EventPage() {
+  const [isMobile, setIsMobile] = useState(false);
   const [width, setWidth] = React.useState();
   const breakpoint = 720;
-
   useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => window.removeEventListener("resize", handleWindowResize);
+    const parser = new UAParser();
+    const userAgent = window.navigator.userAgent;
+    const result = parser.setUA(userAgent).getResult();
+    const isMobileDevice = /mobile/i.test(result.device.type);
+    setIsMobile(isMobileDevice);
   }, []);
+  // useEffect(() => {
+  //   const handleWindowResize = () => setWidth(window.innerWidth);
+  //   window.addEventListener("resize", handleWindowResize);
 
-  return width < breakpoint ? <MobileEventPage /> : <DesktopEventPage />;
+  //   return () => window.removeEventListener("resize", handleWindowResize);
+  // }, []);
+
+  return isMobile ? <MobileEventPage /> : <DesktopEventPage />;
 }
 export default EventPage;
