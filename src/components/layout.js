4c94179b25836ae519aac6ff12ee9badf4e94499
Ronak Gupta <mailtoronakgupta@gmail.com>
2024-01-05 18:46:45 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,17 @@
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import React from "react";
-``;
+import { ParallaxProvider } from "react-scroll-parallax";
 function Layout({ children }) {
   return (
+    <ParallaxProvider>
+
     <div className="layout ">
       <div className="sidebar fixed z-[9999999]  ">{/* <Sidebar/> */}</div>
       <div className="content flex-1 relative w-full ">{children}</div>
       <Footer />
     </div>
+    </ParallaxProvider>
   );
 }
 
