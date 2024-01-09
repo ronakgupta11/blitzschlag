340a328170e4f95b6c716319b5c2ae3a1779b1ed
Ronak Gupta <mailtoronakgupta@gmail.com>
2024-01-09 14:37:13 +0530

    theme page done

--- src/pages/theme.js
+++ src/pages/theme.js
@@ -1,89 +1,85 @@
-import React from "react"
-// import Menu from '../components/Menu'
-import flower1 from "../../public/assets/team/flower1.png"
-import flower from "../../public/assets/team/flower.png"
+import React from 'react'
+import bg from "../../public/assets/theme/bg.svg"
+import Image from 'next/image'
 import blue from "../../public/assets/theme/blue.png"
 import yelo from "../../public/assets/theme/yelo.png"
 import red from "../../public/assets/theme/red.png"
 import orange from "../../public/assets/theme/orange.png"
-import Image from 'next/image'
-const theme = () => {
+function theme2() {
   return (
-    <div className="bg-[#fffbed] min-h-screen relative w-full text-[#393939] text-lg font-amita flex flex-col items-center justify-center p-4">
-      <Image className="absolute top-0 right-0 rotate-90" src={flower1}/>
-      <div className="flex m-2 w-full items-center justify-between">
-        <Image src ={flower}/>
-        <Image src ={flower}/>
-      </div>
-      <p className="text-[#00293E] font-amita text-2xl my-4 ">
-        Theme
-      </p>
-      <p className="text-justify w-[70%] mx-auto my-6">
-      PADHARO MHARE FEST is a quest to bring to the world the cultural essence of our
-enchanted lands of RAJASTHAN, where every hue comes alive, painting the canvas of
-life with vibrant colors. An enchanting realm where culture seamlessly intertwines with
-elegance, classics gracefully dance with modernity, and the soul embraces the horizon
-with unwavering confidence. We present to you the four pinnacles of this magical realm
-depicting the various tints of human emotions. Love, Divinity, Freedom and Strength.
-Jaipur, the capital city, is adorned with a perfect blend of red’s passion and white’s
-purity, pink symbolizing love, nurture, and compassion. A place where people find
-solace amidst life’s hustle and bustle. Udaipur, the City of Lakes, painted with the
-divine color white, synonymous with purity, untainted by any other hue. Jodhpur, the
-Sun City, dipped into the color of the sea, portraying freedom. As unbounded as the sky,
-it has eternal beauty spread across every horizon of the city. Jaisalmer, an oasis in the
-midst of sand and encompassed with eolian sounds shining as bright as gold, with
-beautiful sun-soaked sand dunes, magnificent forts, and temples.
-      </p>
-      <div className="flex flex-wrap items-center w-[70%] m-auto">
-        <div className=" w-64 m-8">
-<div className="flex space-x-4 items-center my-2">
-    <Image src={yelo}/>
-    <p className="font-semibold text-lg">STRENGTH</p>
-</div>
-        <p>
-        As oasis in the midst of sand and encompassed with eolian sounds shining as bright as gold
-        </p>
-        </div>
-        <div className=" w-64 m-8">
-<div className="flex space-x-4 items-center my-2">
-    <Image src={blue}/>
-    <p className="font-semibold text-lg">FREEDOM</p>
-</div>
-        <p>
-        As unbounded as the sky, it has eternal beauty spread across every horizon of the city
-        </p>
-        </div>
-        <div className=" w-64 m-8">
-<div className="flex space-x-4 items-center my-2">
-    <Image src={red}/>
-    <p className="font-semibold text-lg">DIVINITY</p>
-</div>
-        <p>
-        Painted with divine color of white representing certainty and illumination
-        </p>
-        </div>
-        <div className=" w-64 m-8">
-<div className="flex space-x-4 items-center my-2">
-    <Image src={orange}/>
-    <p className="font-semibold text-lg">LOVE</p>
-</div>
-        <p>
-        Perfect blend of red's passion and white's purity, pink symbolizing love, nurture and compassion
-        </p>
-        </div>  
-      </div>
-      <div className="h-12"></div>
-      <div className="flex m-2 w-full items-center justify-between">
-        <Image src ={flower}/>
-        <Image src ={flower}/>
-      </div>
-      <Image className="absolute bottom-0 left-0 -rotate-90 " src={flower1}/>
+    <div>
+<div className='w-full relative bg-[#FCF2D9]'>
+<Image  src={bg}/>
 
+</div>
+<div className='flex absolute font-sankaran text-center bg-clip-text top-[7%] md:top-[10%] bg-[#360805]  flex-col items-center justify-center space-y-2 w-full'>
+
+        <p className='text-md md:text-2xl text-transparent' >
+            theme
+        </p>
+        <p className='text-2xl md:text-5xl text-transparent'>
+            Padharo Mhare Fest
+        </p>
+</div>
+
+<div className='bg-[#4a0e0a] w-full px-10 py-20 flex flex-wrap items-center justify-center m-auto'>
+
+<div className='w-[220px] text-justify h-[220px] text-black flex flex-col space-y-4 items-center justify-around p-4 bg-[#FCF2D9] rounded-xl shadow-md m-4'>
+    <div className='flex space-x-4 items-center justify-center font-semibold'>
+        <Image src={yelo} >
+
+        </Image>
+        <p className='font-amita text-xl'>Strength</p>
     </div>
+    <p className='text-md w-[70%]'>
+    As oasis in the midst of sand and encompassed with eolian sounds shining as bright as gold
+        
+         </p>
+    
+</div>
+<div className='w-[220px] text-justify h-[220px] text-black flex flex-col space-y-4 items-center justify-around p-4 bg-[#FCF2D9] rounded-xl shadow-md m-4'>
+    <div className='flex space-x-4 items-center justify-center font-semibold'>
+        <Image src={orange} >
+
+        </Image>
+        <p className='font-amita text-xl'>Love</p>
+    </div>
+    <p className='text-md w-[70%]'>
+    Perfect blend of red's passion and white's purity, pink symbolizing love, nurture and compassion
+        
+         </p>
+    
+</div>
+<div className='w-[220px] text-justify h-[220px] text-black flex flex-col space-y-4 items-center justify-around p-4 bg-[#FCF2D9] rounded-xl shadow-md m-4'>
+    <div className='flex space-x-4 items-center justify-center font-semibold'>
+        <Image src={blue} >
+
+        </Image>
+        <p className='font-amita text-xl'>Freedom</p>
+    </div>
+    <p className='text-md w-[70%]'>
+    As unbounded as the sky, it has eternal beauty spread across every horizon of the city
+    </p>
+    
+</div>
+<div className='w-[220px] text-justify h-[220px] text-black flex flex-col space-y-4 items-center justify-around p-4 bg-[#FCF2D9] rounded-xl shadow-md m-4'>
+    <div className='flex space-x-4 items-center justify-center font-semibold'>
+        <Image src={red} >
+
+        </Image>
+        <p className='font-amita text-xl'>Divinity</p>
+    </div>
+    <p className='text-md w-[70%]'>
+    Painted with divine color of white representing certainty and illumination
+        
+         </p>
+    
+</div>
+</div>
 
 
+</div>
   )
-  
-};
+}
 
-export default theme;
+export default theme2
