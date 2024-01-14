c9c0c17a4e90b469024273c80c4b792727d82b43
Ronak Gupta <mailtoronakgupta@gmail.com>
2024-01-14 17:20:07 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,23 +6,35 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import { IoMdVolumeOff ,IoMdVolumeHigh} from "react-icons/io";
 import { useState,useRef } from "react";
 function Layout({ children }) {
-  const [isMuted, setIsMuted] = useState(false);
-  const audioRef = useRef(null);
-  const audio = audioRef.current;
+//   const [isMuted, setIsMuted] = useState(false);
+//   const audioRef = useRef(null);
+//   const audio = audioRef.current;
 
   
-  function toggleMute() {
+//   function toggleMute() {
 
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
-    setIsMuted(!audio.paused);
-}
-useEffect(()=>{
-  audio?.play()
-},[audio])
+//     if (audio.paused) {
+//         audio.play();
+//     } else {
+//         audio.pause();
+//     }
+//     setIsMuted(!audio.paused);
+// }
+
+// useEffect(() => {
+//   const audio = audioRef.current;
+
+//   // Play audio on website load
+//   audio.play().catch((error) => {
+//     // Handle play error, for example, autoplay may be blocked by the browser
+//     console.error("Audio play error:", error);
+//   });
+
+//   return () => {
+//     // Cleanup: Pause audio when the component unmounts
+//     audio.pause();
+//   };
+// }, []);
 
   return (
     <ParallaxProvider>
@@ -32,10 +44,10 @@ useEffect(()=>{
         <Navbar/>
         </div>
         <div className="content flex-1 relative w-full ">{children}</div>
-        <audio ref={audioRef} id="soundEffect" loop>
+        {/* <audio ref={audioRef} id="soundEffect" loop>
     <source src="/song.mp3" type="audio/mp3"/>
-        </audio>
-        <button onClick={toggleMute} className="btn btn-circle fixed bottom-10 left-10 z-[999]">{isMuted?<IoMdVolumeOff size={20}/>:<IoMdVolumeHigh size={20}/>}</button>
+        </audio> */}
+        {/* <button onClick={toggleMute} className="btn btn-circle fixed bottom-10 left-10 z-[999]">{isMuted?<IoMdVolumeOff size={20}/>:<IoMdVolumeHigh size={20}/>}</button> */}
         <Footer />
       </div>
     </ParallaxProvider>
