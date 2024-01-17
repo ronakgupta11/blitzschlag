ba4f3524ccca911c9b567f8870422fbc0363042b
Ronak Gupta <mailtoronakgupta@gmail.com>
2024-01-17 18:38:28 +0530

    Update AdminEventTeams.js

--- src/components/adminComponents/AdminEventTeams.js
+++ src/components/adminComponents/AdminEventTeams.js
@@ -5,6 +5,7 @@ import { Table, TextInput, Label, Button } from "flowbite-react";
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { url } from "@/constants";
+import team from "@/pages/team";
 function AdminEventTeams() {
 
 
@@ -14,7 +15,7 @@ function AdminEventTeams() {
   }
   const handleDelete = (id) => {
     axios
-      .post(`${url}/deleteEvent/${id}`)
+      .post(`${url}/deleteTeam/${id}`)
       .then((d) => {
         toast("deleted", { type: "success" });
       })
@@ -43,8 +44,10 @@ const [data,setData] = useState(null)
           <Table.Head>
             <Table.HeadCell>Sr No.</Table.HeadCell>
             <Table.HeadCell>Team Name</Table.HeadCell>
-            {/* <Table.HeadCell>desc</Table.HeadCell> */}
             <Table.HeadCell>Team Id</Table.HeadCell>
+            <Table.HeadCell>College</Table.HeadCell>
+            <Table.HeadCell>Team Leader Name</Table.HeadCell>
+            <Table.HeadCell>Phone No</Table.HeadCell>
             <Table.HeadCell>total Members</Table.HeadCell>
             <Table.HeadCell>MembersID</Table.HeadCell>
             <Table.HeadCell>status</Table.HeadCell>
@@ -55,7 +58,8 @@ const [data,setData] = useState(null)
           </Table.Head>
           <Table.Body className="divide-y">
             {data?.map((e,k) => {
-              // console.log(e);
+              const team  = e?.team
+              const user = e?.teamLeader
               
               return (
                 <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
@@ -63,25 +67,47 @@ const [data,setData] = useState(null)
                     {k+1}
                   </Table.Cell>
                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    {e?.teamName}
+                    {team?.teamName}
                   </Table.Cell>
-                  <Table.Cell>{e?.teamId}</Table.Cell>
-                  <Table.Cell>{e?.members?.length}</Table.Cell>
-                  <Table.Cell>{ ""}</Table.Cell>
-                  <Table.Cell>{e?.status || ""}</Table.Cell>
-                  <Table.Cell>{e?.payments || ""}</Table.Cell>
+                  <Table.Cell>{team?.teamId}</Table.Cell>
+                  <Table.Cell>{user?.college}</Table.Cell>
+                  <Table.Cell>{user?.name}</Table.Cell>
+                  <Table.Cell>{user?.phone}</Table.Cell>
+                  <Table.Cell>{team?.members?.length}</Table.Cell>
+                  <Table.Cell><ul className="flex flex-col space-y-2">
+{team?.members?.map((p,k)=>{
+ return ( <li className="flex space-x-2" key={k}>
+  <p className="w-40">
+    {p} :
+    </p>
+    </li>)
+})}
+                    </ul>
+                  </Table.Cell>
+                  <Table.Cell>{team?.teamStatus || ""}</Table.Cell>
+                  <Table.Cell>{
+                    <ul className="flex flex-col space-y-2">
+                   
+{team?.payments?.map((p,k)=>{
+ return ( <li className="flex space-x-2" key={k}>
+  <p className="w-40">
+    {p?.blitzId} :
+    </p>
+     <a href={p?.imageUrl} target="_blank" className="text-blue-400">view</a></li>)
+})}
+                    </ul>
+                  
+                  }</Table.Cell>
 
                   <Table.Cell>
-                    <Button onClick={() => handleDelete(e?.teamId)}>Verify</Button>
+                    <Button onClick={() => handleDelete(team?.teamId)}>Verify</Button>
                   </Table.Cell>
                   <Table.Cell>
-                    <Button onClick={() => handleDelete(e?.teamId)}>Delete</Button>
+                    <Button onClick={() => handleDelete(team?.teamId)}>Delete</Button>
                   </Table.Cell>
                   <Table.Cell>
-                    <Button onClick={() => handleDelete(e?.teamId)}>Edit</Button>
+                    <Button onClick={() => handleDelete(team?.teamId)}>Edit</Button>
                   </Table.Cell>
-                  {/* <Table.Cell className='h-24 w-24 overflow-auto'>{e?.desc.split(0,5)}</Table.Cell> */}
-                  {/* <Table.Cell className='h-24 w-24 overflow-auto'>{e.oneliner || ""}</Table.Cell> */}
                 </Table.Row>
               );
             })}
